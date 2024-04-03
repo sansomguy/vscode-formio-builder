4360a86269b5aa6d5fe057c63ec6dc471bc94788
Joshua Sansom-Sherwill <joshwebd@gmail.com>
2024-04-03 21:55:19 +1100

    Added the vite basics. Next going to see if i can compile into something that can be rendered from the webview in vscode

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+/**@type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  ignorePatterns: ["media", "dist", ".eslintrc.cjs"],
+  env: { browser: true, es2020: true },
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "react-refresh"],
+  rules: {
+    semi: [2, "always"],
+    "@typescript-eslint/no-unused-vars": 0,
+    "@typescript-eslint/no-explicit-any": 0,
+    "@typescript-eslint/explicit-module-boundary-types": 0,
+    "@typescript-eslint/no-non-null-assertion": 0,
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+  },
+};
