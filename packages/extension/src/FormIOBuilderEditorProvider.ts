405de2a7c36566d1d24f2447aed087e46eac7d68
Josh Sansom-Sherwill <joshwebd@gmail.com>
2024-04-07 11:13:55 +1000

    Fixed the packaging for the extension so that we can share the extension in the office

--- packages/extension/src/FormIOBuilderEditorProvider.ts
+++ packages/extension/src/FormIOBuilderEditorProvider.ts
@@ -117,28 +117,6 @@ export class FormIOBuilderEditorProvider
 		</body>
 		</html>
 		`;
-    // // Local path to script and css for the webview
-
-    // return /* html */ `
-    // 		<!DOCTYPE html>
-    // 		<html lang="en">
-    // 		<head>
-    // 			<meta charset="UTF-8">
-
-    // 			<!--
-    // 			Use a content security policy to only allow loading images from https or from our extension directory,
-    // 			and only allow scripts that have a specific nonce.
-    // 			-->
-    // 			<meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src ${webview.cspSource}; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
-
-    // 			<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    // 			<title>FormIO Builder</title>
-    // 		</head>
-    // 		<body>
-    //     <div id="root"></div>
-    // 		</body>
-    // 		</html>`;
   }
 
   /**
